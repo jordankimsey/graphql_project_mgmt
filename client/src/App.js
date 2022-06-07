25050e2f092114c06404d5b0dafb368c6c0b9e8d
Jordan Kimsey <jordankimsey@gmail.com>
2022-06-07 11:15:27 -0400

    finished client functionality

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+//Import components
+import Header from "./components/Header";
+import Clients from "./components/Clients";
+import AddClientModal from "./components/AddClientModal";
+
+
+//Set up Apollo GraphQL
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+
+//fixes cache merge error
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache,
+});
+
+function App() {
+  return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
+    <div className='container'>
+      <AddClientModal/>
+      <Clients />
+    </div>
+    </ApolloProvider>
+   </>
+  );
+}
+
+export default App;
